67cdd4a576dd0faaeded91ae321aade43fc8a507
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-08-18 15:54:48 +0200

    Create a login endpoint

--- knowx-back/routes/auth.js
+++ knowx-back/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const pool = new Pool({
     user: 'postgres', // DB user(defined in docker)
     host: 'localhost', // where PostgreSql runs (local machine)
@@ -10,7 +11,7 @@ const pool = new Pool({
     port: 5432,  // PostegreSQL default port
 });
 
-//POST /signup
+// POST /signup
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);  //hash the password
@@ -24,6 +25,27 @@ router.post('/signup', async (req, res) => {
     } catch (err) {
         res.status(400).send('Email already in use')
     }
+});
+
+// POST /auth/login
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    // find the user
+    const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    if (user.rows.length === 0) {
+        return res.status(401).send('Incorrect email or password');
+    }
+
+    // compare passwords
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
+    if (!validPassword) {
+        return res.status(401).send('Incorrect email or password')
+    }
+
+    // generate a JWT
+    const token = jwt.sign({ userId: user.rows[0].id }, 'YOUR_SECRET', { expiresIn: '1h' });
+    res.send({ token });
 });
 
 module.exports = router;
