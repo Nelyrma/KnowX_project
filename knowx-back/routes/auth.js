bf68fceb74a32bbf862bbd447666f4c8c871a1c5
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-08-25 17:49:14 +0200

    Modify the signup route

--- knowx-back/routes/auth.js
+++ knowx-back/routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { error } = require('console');
+
 const pool = new Pool({
     user: 'postgres', // DB user(defined in docker)
     host: 'localhost', // where PostgreSql runs (local machine)
@@ -11,19 +13,32 @@ const pool = new Pool({
     port: 5432,  // PostegreSQL default port
 });
 
-// POST /signup
+// POST /signup (inscription)
+// --------------------------
 router.post('/signup', async (req, res) => {
-    const { email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);  //hash the password
+    const { first_name, last_name, email, password } = req.body;
 
     try {
-        await pool.query(
-            'INSERT INTO users (email, password) VALUES ($1, $2)',
-            [email, hashedPassword]
+        const hashedPassword = await bcrypt.hash(password, 10);  //hash the password
+
+        const result = await pool.query(
+            `INSERT INTO users (first_name, last_name, email, password, skills_offered, skills_wanted)
+            VALUES($1, $2, $3, $4, $5, $6)
+            RETURNING id, first_name, last_name, email`,
+            [first_name, last_name, email, hashedPassword, [], []] // empty skills by default
         );
-        res.status(201).send('Account created !');
+
+        res.status(201).json({
+            message: "✅ Account created!",
+            user: result.rows[0]
+        });
     } catch (err) {
-        res.status(400).send('Email already in use')
+        console.error(err);
+        if (err.code === '23505') {
+            res.status(400).json({ error: "❌ Email already in use" });
+        } else {
+            res.status(500).json({ error: "❌ Something went wrong" })
+        }
     }
 });
 
