60d3bda32eeb878a7abdfecc4dc4de4aa3667386
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-08-27 14:22:06 +0200

    Create the profile page

--- knowx-front/src/Profile.jsx
+++ knowx-front/src/Profile.jsx
@@ -1,5 +1,45 @@
+
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 const Profile = () => {
-    return <h1>Profile in construction</h1>;
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            const token = localStorage.getItem('token');
+            try {
+                const res = await axios.get('http://localhost:3001/auth/profile', {
+                    headers: { Authorization: `Bearer ${token}` }
+                });
+                setUser(res.data);
+            } catch (err) {
+                console.error('Error retrieving profile', err);
+            }
+        };
+        fetchProfile();
+    }, []);
+
+    if (!user) return <div>Loading...</div>;
+
+    return (
+        <div>
+            <h1>Profile of {user.first_name} {user.last_name}</h1>
+            <p>Email: {user.email}</p>
+            <h3>Skills offered:</h3>
+            <ul>
+                {user.skills_offered.map((skill, index) => (
+                    <li key={index}>{skill}</li>
+                ))}
+            </ul>
+            <h3>Desired skills</h3>
+            <ul>
+                {user.skills_wanted.map((skill, index) => (
+                    <li key={index}>{skill}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Profile;
