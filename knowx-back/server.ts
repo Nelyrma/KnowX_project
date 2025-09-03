c1874129708c94b340fd665e9e7dd7b4d0bbe6d2
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-09-03 05:16:43 +0200

    Import the offers route

--- knowx-back/server.ts
+++ knowx-back/server.ts
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const offersRoutes = require('./routes/offers');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use('/offers', offersRoutes);
 
 app.listen(3001, () => {
   console.log('Server running on http://localhost:3001');
